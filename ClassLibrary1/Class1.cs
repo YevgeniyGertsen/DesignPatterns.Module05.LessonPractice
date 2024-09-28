f230dc75f468fd501d817a8079ec826e19a99af7
YevgeniyGertsen <gersen.e.a@gmail.com>
2024-09-28 11:01:35 +0500

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public interface IDocumnet
+    {
+        void Open();
+    }
+    public class Report : IDocumnet
+    {
+        public void Open()
+        {
+            //500
+            Console.WriteLine("Report open!");
+        }
+    }
+    public class Resume : IDocumnet
+    {
+        public void Open()
+        {
+            Console.WriteLine("Resume open!");
+        }
+    }
+    public class Letter : IDocumnet
+    {
+        public void Open()
+        {
+            Console.WriteLine("Letter open!");
+        }
+    }
+
+
+    public abstract class DocumentCreator
+    {
+        public abstract IDocumnet CreateDocument();
+    }
+    public class ReportCreator : DocumentCreator
+    {
+        public override IDocumnet CreateDocument()
+        {
+            return new Report();
+        }
+    }
+    public class ResumeCreator : DocumentCreator
+    {
+        public override IDocumnet CreateDocument()
+        {
+            return new Resume();
+        }
+    }
+    public class LetterCreator : DocumentCreator
+    {
+        public override IDocumnet CreateDocument()
+        {
+            return new Letter();
+        }
+    }
+
+}
---
+++ prac5/Program.cs
@@ -0,0 +1,67 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac5
+{
+    public class Invoice : IDocumnet
+    {
+        public void Open()
+        {
+            Console.WriteLine("Invoice open");
+        }
+    }
+
+    public class InvoiceCreator : DocumentCreator
+    {
+        public override IDocumnet CreateDocument()
+        {
+            return new Invoice();
+        }
+    }
+
+
+
+    public enum DocType
+    {
+        Report, Resume, letter, Invoce
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetDocumnet(DocType.Report).Open();
+        }
+
+        public static IDocumnet GetDocumnet(DocType docType)
+        {
+            DocumentCreator creator = null;
+            IDocumnet document = null;
+
+            switch (docType)
+            {
+                case DocType.Report:
+                    creator = new ReportCreator();
+                    break;
+                case DocType.Resume:
+                    creator = new ResumeCreator();
+                    break;
+                case DocType.letter:
+                    creator = new LetterCreator();
+                    break;
+                case DocType.Invoce:
+                    creator = new InvoiceCreator();
+                    break;
+                default:
+                    throw new Exception("Не корректный тип");
+            }
+            document = creator.CreateDocument();
+            return document;
+        }
+
+    }
+}
